80b87d43cce5abdb4cd587394f102c4397a93006
najatessaghir <103741581+najatessaghir@users.noreply.github.com>
2024-06-29 16:16:59 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-docker build -t Chat .
-docker run --name MyApp -t Chat
+pip install -r -U requirements.txt
 
+curl -fsSL https://ollama.com/install.sh | sh
 
+ollama pull mistral
+
+ollama serve
+
+python manage.py runserver
